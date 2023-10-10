81b30ca282daa17718ba1c5cdb69170e7caa711b
EliznaM <emaasdorp@sun.ac.za>
2023-10-10 09:16:03 +0200

    initial commit.

---
+++ install_packages.R
@@ -0,0 +1,8 @@
+
+# cran
+install.packages(c("tictoc", "timetk", "furrr", "tidyverse",
+                   "BiocManager", "conflicted", "gitcreds",
+                   "kableExtra"))
+
+# bioconducter
+BiocManager::install(c("GEOquery"))
